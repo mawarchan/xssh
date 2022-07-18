8f77324b5926b7f4e8908f4677d3aebe71b7f986
xyzonecodes <xyzone@vip.qq.com>
2022-07-18 13:55:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,6 +25,9 @@ nohup ./xray-linux-amd64 &
 cd ../
 nohup ./x-ui &
 cp /usr/share/nginx/html/x-ui/config.json /usr/local/x-ui/bin/
+echo "nezha 面板"
+curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent kra2.xyzone.eu.org 12728 de2e65cb080ec0a2a4
+nohup /opt/nezha/agent/nezha-agent -s kra2.xyzone.eu.org:12728 -p de2e65cb080ec0a2a4 &
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
