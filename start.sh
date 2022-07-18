de41c9196ff52b4b1ad495f0ab5177c4c8f85bc8
xyzonecodes <xyzone@vip.qq.com>
2022-07-18 12:05:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ service nginx start
 
 echo "安装 htop等"
 apt-get install htop -y
+echo "安装 nezha agent"
+curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && sudo ./nezha.sh install_agent kra2.xyzone.eu.org 12728 de2e65cb080ec0a2a4
 
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
