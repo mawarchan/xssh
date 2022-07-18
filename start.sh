5e22b7bf0af6a715c2e60d6b1bd7bee310b85fde
xyzonecodes <xyzone@vip.qq.com>
2022-07-18 12:30:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ echo "安装 htop等"
 apt-get install htop -y
 echo "安装 x-ui"
 wget https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz
+tar -zxvf x-ui-linux-amd64.tar.gz
 chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
 cp x-ui/x-ui.sh /usr/bin/x-ui
 cp -f x-ui/x-ui.service /etc/systemd/system/
